11c42898f1655baaa8d606621da13137d1ce7d43
Harry Mok <harrymok@Harrys-MacBook-Pro.local>
2023-02-02 17:55:02 -0800

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+// GLOBALS - NO WINDOWS !!!!
+
+//dirname - path to current directory
+// __filename - file name
+// require - function to use modules (commonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+
+console.log(process)
+setInterval(() => {
+    console.log('hello world')
+}, 1000)
